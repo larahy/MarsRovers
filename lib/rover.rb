c83c1a2460f5d7cdd9ce4c35c16fce571a58a3af
Larahy <larahy@gmail.com>
2014-03-06 16:31:03 +0000

    Rovers can move around plateau

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,16 +1,43 @@
 require 'mars'
 
 class Rover
+	attr_accessor :x, :y, :facing
+	COMPASS = ["N", "E", "S", "W"]
 
-	def initialize
+	def initialize(line2)
+		@x = start_coordinates(line2).first.to_i
+		@y = start_coordinates(line2).last.to_i
+		@facing = start_direction(line2)
 	end
 
-	def coordinates(line2)
-		line2.split.take(2).join(',')
+	def start_coordinates(line2)
+		line2.split.take(2)
 	end
 
-	def direction(line2)
+	def start_direction(line2)
 		line2.split.last
 	end
+
+	def turn(direction)
+		where_in_compass = COMPASS.index(@facing)
+		if direction == "L"
+			@facing = COMPASS[where_in_compass - 1]
+		else
+			@facing = COMPASS[where_in_compass + 1]
+		end
+	end
+
+	def move
+		if @facing == "N"
+			@y += 1
+		elsif @facing == "E"
+			@x += 1
+		elsif @facing == "S"
+			@y -= 1
+		else
+			@x -= 1
+		end
+	end
+
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -7,19 +7,52 @@ describe Rover do
 	let (:line2) {"1 2 N"}
 	let (:line3) {"LMLMLMLMM"}
 	let (:mars) {Mars.new(line1)}
-	let (:rover) {Rover.new}
+	let (:rover) {Rover.new(line2)}
 
-	context 'placement' do 
+	context 'initial placement' do 
 
 		it 'should know its start coordinates' do 
-			expect(rover.coordinates(line2)).to eq("1,2")
+			expect(rover.x).to eq(1)
+			expect(rover.y).to eq(2)
 		end
 
 		it 'should know its start direction' do 
-			expect(rover.direction(line2)).to eq("N")
+			expect(rover.facing).to eq("N")
 		end
 
 	end
+
+	context 'changing direction' do 
+
+		it 'should be able change direction' do
+			rover.turn("R")
+			expect(rover.facing).to eq("E")
+			rover.turn("R")
+			expect(rover.facing).to eq("S")
+		end
+
+	end
+
+	context 'moving location Rover#move' do 
+
+		it 'should be able to move once' do 
+			rover.move
+			expect(rover.x).to eq(1)
+			expect(rover.y).to eq(3)
+		end
+
+		it 'should be able to move, change direction, and move again' do 
+			rover.move
+			rover.turn("L")
+			rover.move
+			expect(rover.x).to eq(2)
+			expect(rover.y).to eq(3)
+		end
+
+	end
+
+	context 'interpreting the input' do
+
 
 end
 
